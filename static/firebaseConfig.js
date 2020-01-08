18a5e7a18028d996c4e5ec45012c70558504b06d
Giuliana <giulianaolmos@gmail.com>
2020-01-08 12:43:29 -0300

    first commit

---
+++ static/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCr67wg6lFCNMoUkyaS2GvvKcO9QAhCYqE",
+    authDomain: "nodechallenge-424c1.firebaseapp.com",
+    databaseURL: "https://nodechallenge-424c1.firebaseio.com",
+    projectId: "nodechallenge-424c1",
+    storageBucket: "nodechallenge-424c1.appspot.com",
+    messagingSenderId: "198380806724",
+    appId: "1:198380806724:web:40d57506473d3f31f39211"
+  };
+  firebase.initializeApp(config);
