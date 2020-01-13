425746d4fbd8d8a7b850985468547f0a59396b9a
Giuliana <giulianaolmos@gmail.com>
2020-01-13 16:35:27 -0300

    agregando los ultimos endpoints

--- src/controllers/Sites_Version_1.ts
+++ src/controllers/Sites_Version_1.ts
@@ -1,6 +1,5 @@
 import { Router, Response, NextFunction } from 'express';
-import firebase from '../service/firebase.service';
-import firestore from 'firebase/firestore'
+import {firebase} from '../service/firebase.service';
 
 class SitesVersion1 {
 
@@ -16,16 +15,143 @@ class SitesVersion1 {
       res.status(200).json({ success: true });
     });
     this.router.get('/get_sites/:email/:password', (req: any, res: Response, next: NextFunction) => this.getSites(req, res, next));
+    this.router.get('/get_sites_just_name_id/:email/:password', (req: any, res: Response, next: NextFunction) => this.getSitesByName(req, res, next));
+    this.router.get('/get_sites_by_name/:email/:password/:name', (req: any, res: Response, next: NextFunction) => this.getSitesById(req, res, next));
+    this.router.get('/get_sites_by_id/:email/:password/:id', (req: any, res: Response, next: NextFunction) => this.getSitesById(req, res, next));
+    this.router.get('/update_sites_id/:email/:password/:description/:id/:name/:lat/:long/:image', (req: any, res: Response, next: NextFunction) => this.updateSites(req, res, next));
+    this.router.get('/delete_sites_by_id/:email/:password/:id', (req: any, res: Response, next: NextFunction) => this.deleteSitesById(req, res, next));
   };
 
+
+  //Obtener todos los sitios
   async getSites(req: any, res: Response, next: NextFunction) {
     try {
       const { params: { email, password } } = req;
-      //const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
-      const db = await firebase.firestore().colletion('SitiosInteres');
-      const sites = await db.add({ id : "iofhaoi" });
-      const site = sites.get();
-      return res.status(200).json(site);
+      const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const ref = firebase.firestore()
+      const documents = await (await ref.collection('SitiosInteres').get()).docs 
+      var sitios = []
+      for(var i = 0; i< documents.length; i++){
+        sitios.push(documents[i].data())
+      }
+      console.log(sitios) 
+      return res.status(200).json(sitios)
+    } catch (error) {
+      if(error === 'unaturozide'){
+        return res.status(400).json({ error });
+      }
+      return res.status(500).json({ error });
+    }
+  }
+
+  //Obtener todos los sitios solo nombre y ID
+  async getSitesJustNameAndId(req: any, res: Response, next: NextFunction) {
+    try {
+      const { params: { email, password } } = req;
+      const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const ref = firebase.firestore()
+      const documents = await (await ref.collection('SitiosInteres').get()).docs 
+      var sitiosFiltrados = documents.map( function(document){
+        var sitio = {
+        id : document.data().id,
+        nombre: document.data().nombre,
+        }
+        return sitio;
+      })
+      console.log(sitiosFiltrados) 
+      return res.status(200).json(sitiosFiltrados)
+    } catch (error) {
+      if(error === 'unaturozide'){
+        return res.status(400).json({ error });
+      }
+      return res.status(500).json({ error });
+    }
+  }
+
+  //Filtrar los sitios por Nombre
+  async getSitesByName(req: any, res: Response, next: NextFunction) {
+    try {
+      const { params: { email, password, name } } = req;
+      const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const ref = firebase.firestore()
+      const documents = await (await ref.collection('SitiosInteres').get()).docs 
+      var sitiosFiltrados = documents.filter(function(document){
+        return document.data().nombre.indexOf(name);
+      })
+      console.log(sitiosFiltrados) 
+      return res.status(200).json(sitiosFiltrados)
+    } catch (error) {
+      if(error === 'unaturozide'){
+        return res.status(400).json({ error });
+      }
+      return res.status(500).json({ error });
+    }
+  }
+
+  //Obtener solo los sitios por ID
+  async getSitesById(req: any, res: Response, next: NextFunction) {
+    try {
+      const { params: { email, password, id } } = req;
+      const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const ref = firebase.firestore()
+      const documents = await (await ref.collection('SitiosInteres').get()).docs 
+      console.log(documents[id].data()) 
+      return res.status(200).json(documents[id].data())
+    } catch (error) {
+      if(error === 'unaturozide'){
+        return res.status(400).json({ error });
+      }
+      return res.status(500).json({ error });
+    }
+  }
+
+  //Crear y modificar sitios por id
+  async updateSites(req: any, res: Response, next: NextFunction) {
+    try {
+      const { params: { email, password, description, id, name, lat, long, image } } = req;
+      const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const ref = firebase.firestore();
+      var sitio = {
+        descripcion : description, 
+        id : parseInt(id),
+        nombre : name,
+        ubicacion : {
+          _lat : parseFloat(lat),
+          _long : parseFloat(long),
+        },
+        url_imagen : image
+      }
+      var sitioUpdated = await ref.collection('SitiosInteres').doc(id).set(sitio);
+
+      console.log(sitio) 
+      return res.status(200).json(sitio)
+    } catch (error) {
+      if(error === 'unaturozide'){
+        return res.status(400).json({ error });
+      }
+      return res.status(500).json({ error });
+    }
+  }
+
+  async deleteSitesById(req: any, res: Response, next: NextFunction) {
+    try {
+      const { params: { email, password, description, id, name, lat, long, image } } = req;
+      const auth = await firebase.auth().signInWithEmailAndPassword(email, password);
+      const ref = firebase.firestore();
+      var sitio = {
+        descripcion : description, 
+        id : parseInt(id),
+        nombre : name,
+        ubicacion : {
+          _lat : parseFloat(lat),
+          _long : parseFloat(long),
+        },
+        url_imagen : image
+      }
+      var sitioUpdated = await ref.collection('SitiosInteres').doc(id).set(sitio);
+
+      console.log(sitio) 
+      return res.status(200).json(sitio)
     } catch (error) {
       if(error === 'unaturozide'){
         return res.status(400).json({ error });
--- src/service/firebase.service.ts
+++ src/service/firebase.service.ts
@@ -3,11 +3,7 @@ import firebase from 'firebase';
 //  var functions = require('firebase-functions')
 
 
-class Firebase {
-  
-  firebase: any;
-
-  config = {
+  const config = {
     apiKey: "AIzaSyCr67wg6lFCNMoUkyaS2GvvKcO9QAhCYqE",
     authDomain: "nodechallenge-424c1.firebaseapp.com",
     databaseURL: "https://nodechallenge-424c1.firebaseio.com",
@@ -17,11 +13,10 @@ class Firebase {
     appId: "1:198380806724:web:40d57506473d3f31f39211"
   };
 
-  constructor(){
-    this.firebase = firebase.initializeApp(this.config);
-  }
 
-}
-const firebaseInstance = new Firebase()
+  firebase.initializeApp(config);
 
-export default firebaseInstance.firebase;
+
+
+
+export {firebase}
